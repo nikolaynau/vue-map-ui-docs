0c4c6578b07021c70ff14be4738399df3e5a220c
nikolaynau <nikolaynau@users.noreply.github.com>
2023-02-07 16:34:00 +0000

    deploy: a3f689184d6911457d7da17c1e465283219aa115

---
+++ assets/index.html.43798fd7.js
@@ -0,0 +1,9 @@
+import{_ as n,o as a,c as e,a as s}from"./app.df857707.js";const i={},l=s(`<h2 id="installation" tabindex="-1"><a class="header-anchor" href="#installation" aria-hidden="true">#</a> Installation</h2><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code><span class="token comment"># NPM</span>
+$ <span class="token function">npm</span> <span class="token function">install</span> vue3-map-ui leaflet
+
+<span class="token comment"># Yarn</span>
+$ <span class="token function">yarn</span> <span class="token function">add</span> vue3-map-ui leaflet
+
+<span class="token comment"># pnpm</span>
+$ <span class="token function">pnpm</span> <span class="token function">install</span> vue3-map-ui leaflet
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="usage" tabindex="-1"><a class="header-anchor" href="#usage" aria-hidden="true">#</a> Usage</h2>`,3),t=[l];function c(d,o){return a(),e("div",null,t)}var u=n(i,[["render",c],["__file","index.html.vue"]]);export{u as default};
