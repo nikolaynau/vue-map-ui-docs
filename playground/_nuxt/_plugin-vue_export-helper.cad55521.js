954da25582c7a2a6208a261de0044bfbafdd847a
nikolaynau <nikolaynau@users.noreply.github.com>
2023-03-05 18:45:27 +0000

    deploy: 9f789d40d2dd10ce6c1b86cfca290228efbd292e

---
+++ playground/_nuxt/_plugin-vue_export-helper.cad55521.js
@@ -0,0 +1,1 @@
+import{O as r,V as u}from"./entry.fe39cdaf.js";function f(t,e){return r()._useHead(t,e)}const a={};u(a);const p=(t,e)=>{const n=t.__vccOpts||t;for(const[o,s]of e)n[o]=s;return n};export{p as _,f as u};
