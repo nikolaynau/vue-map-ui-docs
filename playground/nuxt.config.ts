929a67ebc33492f4ca0546b4043026892e5dcd3e
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2023-09-05 19:50:32 +0300

    fix: base url

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
+      baseURL: '/playground/',
       openTransportApiKey: 'af7fd7de0ed048059413e19e7fb7d90b',
       mapboxAccessToken:
         'pk.eyJ1IjoiYWx0YXJpY2thIiwiYSI6ImNpa3BidXNoaTExMWh1Mm02YTY1ZXlvZXkifQ.fRS4SvpNrcqc7ZFKQSUJxA'
--- playground/pages/layer/basic-icon.vue
+++ playground/pages/layer/basic-icon.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+const baseUrl = runtimeConfig.public.baseURL;
+</script>
+
 <template>
   <VMap>
     <VMapOsmTileLayer />
@@ -5,7 +10,7 @@
     <VMapAttributionControl />
     <VMapMarker :latlng="[0, 0]">
       <VMapIcon
-        icon-url="/custom/marker-icon.png"
+        :icon-url="`${baseUrl}custom/marker-icon.png`"
         :icon-size="[32, 42]"
         :icon-anchor="[16, 42]"
       />
